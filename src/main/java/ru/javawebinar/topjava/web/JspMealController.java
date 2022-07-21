4b3897af9733a95698cfe0f034a3254e588929a6
SagaOfYamato <yamato-rebellion@yandex.ru>
2022-07-21 09:43:45 +0300

    Prepare to HW7 - patch_7.01

--- src/main/java/ru/javawebinar/topjava/web/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspMealController.java
@@ -5,12 +5,27 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.UnsupportedEncodingException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
+@RequestMapping(value = "/meals")
 public class JspMealController {
 
     private static final Logger log = LoggerFactory.getLogger(JspMealController.class);
@@ -18,14 +33,73 @@ public class JspMealController {
     @Autowired
     private MealService service;
 
-    @GetMapping("/meals")
-    public String getAll(Model model, HttpServletRequest request) {
-        String action = request.getParameter("action");
-        log.info("users");
-        if (action == null) {
+    @GetMapping()
+    public String getAll(Model model) {
         int userId = SecurityUtil.authUserId();
-        model.addAttribute("meals", service.getAll(userId));
+        log.info("getAll for user {}", userId);
+        model.addAttribute("meals", MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
+    }
+
+   /* @GetMapping("/meals")
+    public String get(HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
+        int id = getId(request);
+        log.info("get meal {} for user {}", id, userId);
+        service.get(id, userId);
+        return "meals";
+    }*/
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable String id) {
+        int userId = SecurityUtil.authUserId();
+        log.info("delete meal {} for user {}", id, userId);
+        service.delete(Integer.parseInt(id), userId);
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+        model.addAttribute("meal", meal);
+        return "/mealForm";
+    }
+
+    @GetMapping("/update/{id}")
+    public String update(Model model, @PathVariable String id) {
+        int userId = SecurityUtil.authUserId();
+        Meal meal = service.get(Integer.parseInt(id), userId);
+        log.info("update {} for user {}", meal, userId);
+        model.addAttribute("meal", meal);
+        return "/mealForm";
+    }
+
+    @PostMapping()
+    public String updateOrCreate(HttpServletRequest request) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("UTF-8");
+        String id = request.getParameter("id");
+        int userId = SecurityUtil.authUserId();
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+        if (StringUtils.hasLength(id)) {
+            service.update(meal, userId);
+        } else {
+            service.create(meal, userId);
         }
-        return "meals.jsp";
+        return "redirect:/meals";
+    }
+
+    @GetMapping("/filter")
+    public String getBetween(Model model, HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", MealsUtil.getFilteredTos(service.getBetweenInclusive(startDate, endDate, userId),
+                SecurityUtil.authUserCaloriesPerDay(), startTime, endTime));
+        return "redirect:/meals";
     }
 }
